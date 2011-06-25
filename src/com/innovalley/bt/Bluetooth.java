d5eeb97e9c0a2c46b3814256a085bd4c88fa30cd
Jesus Alonso <jesus@innovalley.us>
2011-06-24 17:37:18 -0700

    Friday's commit - dangerous maybe

--- src/com/innovalley/bt/Bluetooth.java
+++ src/com/innovalley/bt/Bluetooth.java
@@ -2,72 +2,63 @@ package com.innovalley.bt;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Set;
 import java.util.UUID;
-
-import android.app.AlertDialog;
-import android.app.Application;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.DialogInterface;
 
-public class Bluetooth{ 
-	private String address;
-	private BluetoothAdapter mBluetoothAdapter = null;
-	private BluetoothSocket btSocket = null;
-	private OutputStream outStream = null;
+public class Bluetooth {
+	
+	// Local device Bluetooth adapter
+	private final BluetoothAdapter mBluetoothAdapter;
+	
+	// Remote device
+	private BluetoothDevice remoteDevice;
+	
+	// Bluetooth socket - RFCOMM
+	private BluetoothSocket btSocket;
+	
+	// A means of writing data to a target in a byte-wise manner.
+	private OutputStream outStream;
+	
+    // Constants that indicate the current connection state
+    public static final int STATE_NONE = 0;       // we're doing nothing
+    public static final int STATE_CONNECTING = 1; // now initiating an outgoing connection
+    public static final int STATE_CONNECTED = 2;  // now connected to a remote device
+	
 	private boolean connected;
-	private Application application;
+
 	private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
 	public Bluetooth(String macAddress) {
-		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		this.address = macAddress;
-		this.openSocket();
-		this.connected = false;
-	}
-
-	public boolean isConnected() {
-		return connected;
-	}
-
-	public void setConnected(boolean connected) {
-		this.connected = connected;
-	}
-
-	public boolean isEnabled() {
-		return this.mBluetoothAdapter.isEnabled();
-	}
-
-	public void openSocket() {
-		BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter(); // Local
+		remoteDevice = mBluetoothAdapter.getRemoteDevice(macAddress); // Remote
 		try {
-			if (device == null) throw new Exception("It couldn't be connected with the device" + this.address);
-			btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+			if (remoteDevice == null)
+				throw new Exception("It couldn't be connected with the device "	+ macAddress);
+			btSocket = remoteDevice.createRfcommSocketToServiceRecord(MY_UUID);
 			mBluetoothAdapter.cancelDiscovery();
 		} catch (Exception e) {
 		}
+		connected = false;
 	}
 
 	public void connect() throws Exception {
 		try {
 			btSocket.connect();
-			this.connected = true;
+			connected = true;
 		} catch (Exception e) {
 			try {
 				btSocket.close();
 			} catch (Exception e2) {
-				throw new Exception("ON RESUME: Unable to close socket during connection failure" + e2.getMessage());
+				throw new Exception("Unable to close socket during connection failure" + e2.getMessage());
 			}
 		}
 
 		try {
 			outStream = btSocket.getOutputStream();
 		} catch (IOException e) {
-			throw new Exception(
-					"BLUETOOTH CONSTRUCTOR:Output stream creation failed."
-							+ e.getMessage());
+			throw new Exception("Output stream creation failed." + e.getMessage());
 		}
 	}
 
@@ -77,48 +68,29 @@ public class Bluetooth{
 			outStream.write(msgBuffer);
 			outStream.flush();
 		} catch (Exception e) {
-			throw new Exception("BLUETOOTH SEND MESSAGE:Exception during write."+ e.getMessage());
+			throw new Exception("BLUETOOTH SEND MESSAGE:Exception during write." + e.getMessage());
 		}
 	}
 
 	public void disconnect() throws Exception {
 		try {
-			this.btSocket.close();
-			this.outStream.close();
-			this.connected = false;
+			btSocket.close();
+			outStream.close();
+			connected = false;
 		} catch (Exception e2) {
-			throw new Exception("ON RESUME: Unable to close socket during connection failure" + e2.getMessage());
+			throw new Exception("Unable to close socket during connection failure" + e2.getMessage());
 		}
 	}
 
-	public void pair() {
-		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();		
-		AlertDialog.Builder ab = new AlertDialog.Builder(this.application.getApplicationContext());
-		ab.setTitle("Pairment");
-		String[] data = new String[pairedDevices.size()];
-		int i = 0;
-		// If there are paired devices
-		if (pairedDevices.size() > 0) {
-			// Loop through paired devices
-			for (BluetoothDevice device : pairedDevices) {
-				data[i] = device.getName() + "\n" + device.getAddress();
-				i++;
-			}
-		}
+	public boolean isConnected() {
+		return connected;
+	}
 
-		ab.setSingleChoiceItems(data, 0, new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int whichButton) {
+	public void setConnected(boolean conn) {
+		connected = conn;
+	}
 
-			}
-		}).setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int whichButton) {
-			}
-		}).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int whichButton) {
-				// on cancel button action
-			}
-		});
-		ab.show();
-
+	public boolean isEnabled() {
+		return mBluetoothAdapter.isEnabled();
 	}
 }
--- src/com/innovalley/bt/BluetoothManager.java
+++ src/com/innovalley/bt/BluetoothManager.java
@@ -3,15 +3,15 @@ import android.os.SystemClock;
 
 public class BluetoothManager extends Thread {
 
-	private Bluetooth left = null;
+	private Bluetooth btDevice;
 	private static final int timeBlock = 500;
 
-	public BluetoothManager(String mac) throws Exception {
-		this.left = new Bluetooth(mac);
+	public BluetoothManager(String mac){
+		btDevice = new Bluetooth(mac);
 	}
 
 	public boolean isBtEnabled() {
-		if (this.left.isEnabled())return true;
+		if (btDevice.isEnabled()) return true;
 		return false;
 	}
 
@@ -23,39 +23,72 @@ public class BluetoothManager extends Thread {
 		return true;
 	}
 
-	public void turnLeft() throws Exception {
-		this.left.sendMessage("CMD=0,002,050,01,100\\r");
+	public void send(String message) throws Exception {
+		this.btDevice.sendMessage(message);
 		SystemClock.sleep(timeBlock);
 	}
 
 	public void reset() throws Exception {
 		disconnect();
-		this.left.connect();
-		this.left.sendMessage("CMD=1\n\r");
+		this.btDevice.connect();
+		this.btDevice.sendMessage("CMD=1\n\r");
 		SystemClock.sleep(timeBlock);
 	}
 
 	public void pairDevice(int pairShoe) {
-		this.left.pair();
+		//this.btDevice.pair();
 	}
 
-	public void disconnect() throws Exception {
-		this.left.sendMessage("CMD=2\n\r");
-		this.left.disconnect();
+	public void disconnect() {	
+		try {
+		btDevice.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private void connect() {
 		try {
-			this.left.connect();
+			this.btDevice.connect();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		try {
-			this.left.sendMessage("CMD=1\n\r");
+			this.btDevice.sendMessage("CMD=1\n\r");
 			SystemClock.sleep(timeBlock);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+//	public void pair() {
+//		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+//		AlertDialog.Builder ab = new AlertDialog.Builder(this.application.getApplicationContext());
+//		ab.setTitle("Pairment");
+//		String[] data = new String[pairedDevices.size()];
+//		int i = 0;
+//		// If there are paired devices
+//		if (pairedDevices.size() > 0) {
+//			// Loop through paired devices
+//			for (BluetoothDevice device : pairedDevices) {
+//				data[i] = device.getName() + "\n" + device.getAddress();
+//				i++;
+//			}
+//		}
+//
+//		ab.setSingleChoiceItems(data, 0, new DialogInterface.OnClickListener() {
+//			public void onClick(DialogInterface dialog, int whichButton) {
+//
+//			}
+//		}).setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+//			public void onClick(DialogInterface dialog, int whichButton) {
+//			}
+//		}).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//			public void onClick(DialogInterface dialog, int whichButton) {
+//				// on cancel button action
+//			}
+//		});
+//		ab.show();
+//	}
 	
 }
